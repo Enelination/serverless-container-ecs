ecfa7c0c4b518efd9146c1415c5bf4c0e66c660d
enel <enel@gmail.com>
2022-07-08 14:23:51 +0000

    v1.0.0

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "containerc2abe80c": {
+            "ServiceName": "string",
+            "ClusterName": "string",
+            "PipelineName": "string",
+            "ContainerNames": "string",
+            "ApiName": "string",
+            "RootUrl": "string"
+        }
+    }
+}
